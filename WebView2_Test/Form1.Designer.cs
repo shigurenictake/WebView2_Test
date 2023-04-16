e63bd32e83561b49dafaebf25a033f7a477d194e
shigurenic8 <98692208+shigurenic8@users.noreply.github.com>
2023-04-16 22:37:54 +0900

    first commit
    C#とJavaScript連携

---
+++ WebView2_Test/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WebView2_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.textBox_UrlBar = new System.Windows.Forms.TextBox();
+            this.label_CsToJs = new System.Windows.Forms.Label();
+            this.label_JsToCs = new System.Windows.Forms.Label();
+            this.richTextBox_JsToCs = new System.Windows.Forms.RichTextBox();
+            this.button_CsToJs = new System.Windows.Forms.Button();
+            this.label_UrlBar = new System.Windows.Forms.Label();
+            this.textBox_CsToJs = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(12, 37);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(643, 401);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // textBox_UrlBar
+            // 
+            this.textBox_UrlBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_UrlBar.Location = new System.Drawing.Point(46, 12);
+            this.textBox_UrlBar.Name = "textBox_UrlBar";
+            this.textBox_UrlBar.Size = new System.Drawing.Size(609, 19);
+            this.textBox_UrlBar.TabIndex = 1;
+            this.textBox_UrlBar.TextChanged += new System.EventHandler(this.textBox_UrlBar_TextChanged);
+            // 
+            // label_CsToJs
+            // 
+            this.label_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_CsToJs.AutoSize = true;
+            this.label_CsToJs.Location = new System.Drawing.Point(661, 37);
+            this.label_CsToJs.Name = "label_CsToJs";
+            this.label_CsToJs.Size = new System.Drawing.Size(126, 12);
+            this.label_CsToJs.TabIndex = 2;
+            this.label_CsToJs.Text = "◆C#からJavaScriptへ＞";
+            // 
+            // label_JsToCs
+            // 
+            this.label_JsToCs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_JsToCs.AutoSize = true;
+            this.label_JsToCs.Location = new System.Drawing.Point(659, 139);
+            this.label_JsToCs.Name = "label_JsToCs";
+            this.label_JsToCs.Size = new System.Drawing.Size(165, 12);
+            this.label_JsToCs.TabIndex = 4;
+            this.label_JsToCs.Text = "◆JavaScriptから受け取った値＞\r\n";
+            // 
+            // richTextBox_JsToCs
+            // 
+            this.richTextBox_JsToCs.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox_JsToCs.Location = new System.Drawing.Point(662, 154);
+            this.richTextBox_JsToCs.Name = "richTextBox_JsToCs";
+            this.richTextBox_JsToCs.Size = new System.Drawing.Size(345, 284);
+            this.richTextBox_JsToCs.TabIndex = 5;
+            this.richTextBox_JsToCs.Text = "";
+            // 
+            // button_CsToJs
+            // 
+            this.button_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_CsToJs.Location = new System.Drawing.Point(661, 77);
+            this.button_CsToJs.Name = "button_CsToJs";
+            this.button_CsToJs.Size = new System.Drawing.Size(66, 23);
+            this.button_CsToJs.TabIndex = 6;
+            this.button_CsToJs.Text = "▼送信";
+            this.button_CsToJs.UseVisualStyleBackColor = true;
+            this.button_CsToJs.Click += new System.EventHandler(this.button_CsToJs_Click);
+            // 
+            // label_UrlBar
+            // 
+            this.label_UrlBar.AutoSize = true;
+            this.label_UrlBar.Location = new System.Drawing.Point(13, 19);
+            this.label_UrlBar.Name = "label_UrlBar";
+            this.label_UrlBar.Size = new System.Drawing.Size(27, 12);
+            this.label_UrlBar.TabIndex = 7;
+            this.label_UrlBar.Text = "URL";
+            // 
+            // textBox_CsToJs
+            // 
+            this.textBox_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_CsToJs.Location = new System.Drawing.Point(661, 52);
+            this.textBox_CsToJs.Name = "textBox_CsToJs";
+            this.textBox_CsToJs.Size = new System.Drawing.Size(346, 19);
+            this.textBox_CsToJs.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 450);
+            this.Controls.Add(this.textBox_CsToJs);
+            this.Controls.Add(this.label_UrlBar);
+            this.Controls.Add(this.button_CsToJs);
+            this.Controls.Add(this.richTextBox_JsToCs);
+            this.Controls.Add(this.label_JsToCs);
+            this.Controls.Add(this.label_CsToJs);
+            this.Controls.Add(this.textBox_UrlBar);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.TextBox textBox_UrlBar;
+        private System.Windows.Forms.Label label_CsToJs;
+        private System.Windows.Forms.Label label_JsToCs;
+        private System.Windows.Forms.Button button_CsToJs;
+        private System.Windows.Forms.Label label_UrlBar;
+        public System.Windows.Forms.RichTextBox richTextBox_JsToCs;
+        private System.Windows.Forms.TextBox textBox_CsToJs;
+    }
+}
+
---
+++ WebView2_Test/Form1.cs
@@ -0,0 +1,113 @@
+﻿
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebView2_Test
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>webviewのコントロール（わかりやすい様に、デザイナーを使わずにコード側で実装します。）</summary>
+        //private Microsoft.Web.WebView2.WinForms.WebView2 webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+
+        /// <summary>JavaScriptで呼ぶ関数を保持するオブジェクト</summary>
+        private JsToCs CsClass = new JsToCs();
+
+        public Form1()
+        {
+            //JsToCsクラスでForm1のフォーム取得参照用
+            CsClass.form1 = this;
+
+            //this.Controls.Add(webView);
+            InitializeComponent();
+
+            //URLを設定
+            this.textBox_UrlBar.Text = System.IO.Path.GetFullPath(@"..\\..\\html\\sample.html");
+            this.webView.Source = new Uri(this.textBox_UrlBar.Text);
+
+            //WebView2のサイズをフォームのサイズに合わせる
+            //webView.Size = this.Size;
+            //this.SizeChanged += Form1_SizeChanged;
+
+            //WebView2のロード完了時のイベント
+            webView.NavigationCompleted += WebView_NavigationCompleted;
+        }
+
+        /// <summary>WebView2のロード完了時</summary>
+        private void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            try
+            {
+                if (webView.CoreWebView2 != null)
+                {
+                    //JavaScriptからC#のメソッドが実行できる様に仕込む
+                    webView.CoreWebView2.AddHostObjectToScript("CsClass", CsClass);
+                }
+                else MessageBox.Show("CoreWebView2==null");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //イベント - URL変更
+        private void textBox_UrlBar_TextChanged(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(this.textBox_UrlBar.Text);
+            }
+        }
+
+        //イベント - C#からJavaScriptへ送信ボタン
+        private async void button_CsToJs_Click(object sender, EventArgs e)
+        {
+            string sendStr = this.textBox_CsToJs.Text;
+
+
+            //JavaScriptの関数を実行
+            //WebView.ExecuteScriptAsync("func1()").ResultをするとWebView2がフリーズする
+            string str1 = await webView.ExecuteScriptAsync("jsFunc1(\"" + sendStr + "\")");
+            this.richTextBox_JsToCs.AppendText("Jsからの戻り値＞" + str1 + "\n");
+        }
+
+
+        /// <summary>サイズ変更時のイベントでWebView2のサイズをフォームに合わせる</summary>
+        //private void Form1_SizeChanged(object sender, EventArgs e)
+        //{
+        //    webView.Size = this.Size;
+        //}
+    }
+
+    //↓属性設定が無いとエラーになります
+    /// <summary>WebView2に読み込ませるためのJsで実行する関数を保持させたクラス</summary>
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public class JsToCs
+    {
+        public Form1 form1;
+
+        /*        public JsToCs(Form1 form1)
+                {
+                    this.form1 = new Form1();
+                }*/
+
+        //Jsからの呼び出し
+        public void JsToCsMethod(string strText)
+        {
+            form1.richTextBox_JsToCs.AppendText(strText + "\n");
+        }
+    }
+}
