5e0d2ad3efe97be037b64643c96d117317d9181f
shigurenic8 <98692208+shigurenic8@users.noreply.github.com>
2023-04-23 23:39:06 +0900

    Revert "SharpMap 導入"
    This reverts commit 21feb868291846ea6b23565e8dd9cd1c30ce13ec.

--- WebView2_Test/Form1.Designer.cs
+++ WebView2_Test/Form1.Designer.cs
@@ -38,7 +38,6 @@
             this.textBox_CsToJs = new System.Windows.Forms.TextBox();
             this.label_CsToJsReturn = new System.Windows.Forms.Label();
             this.richTextBox_CsToJsReturn = new System.Windows.Forms.RichTextBox();
-            this.mapBox1 = new SharpMap.Forms.MapBox();
             ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +51,7 @@
             this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
             this.webView.Location = new System.Drawing.Point(12, 37);
             this.webView.Name = "webView";
-            this.webView.Size = new System.Drawing.Size(377, 697);
+            this.webView.Size = new System.Drawing.Size(643, 401);
             this.webView.TabIndex = 0;
             this.webView.ZoomFactor = 1D;
             // 
@@ -62,15 +61,15 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox_UrlBar.Location = new System.Drawing.Point(46, 12);
             this.textBox_UrlBar.Name = "textBox_UrlBar";
-            this.textBox_UrlBar.Size = new System.Drawing.Size(249, 19);
+            this.textBox_UrlBar.Size = new System.Drawing.Size(609, 19);
             this.textBox_UrlBar.TabIndex = 1;
             this.textBox_UrlBar.TextChanged += new System.EventHandler(this.textBox_UrlBar_TextChanged);
             // 
             // label_CsToJs
             // 
-            this.label_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_CsToJs.AutoSize = true;
-            this.label_CsToJs.Location = new System.Drawing.Point(725, 411);
+            this.label_CsToJs.Location = new System.Drawing.Point(661, 37);
             this.label_CsToJs.Name = "label_CsToJs";
             this.label_CsToJs.Size = new System.Drawing.Size(126, 12);
             this.label_CsToJs.TabIndex = 2;
@@ -78,9 +77,9 @@
             // 
             // label_JsToCs
             // 
-            this.label_JsToCs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_JsToCs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_JsToCs.AutoSize = true;
-            this.label_JsToCs.Location = new System.Drawing.Point(725, 587);
+            this.label_JsToCs.Location = new System.Drawing.Point(661, 230);
             this.label_JsToCs.Name = "label_JsToCs";
             this.label_JsToCs.Size = new System.Drawing.Size(165, 12);
             this.label_JsToCs.TabIndex = 4;
@@ -88,17 +87,18 @@
             // 
             // richTextBox_JsToCs
             // 
-            this.richTextBox_JsToCs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox_JsToCs.Location = new System.Drawing.Point(726, 602);
+            this.richTextBox_JsToCs.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox_JsToCs.Location = new System.Drawing.Point(662, 245);
             this.richTextBox_JsToCs.Name = "richTextBox_JsToCs";
-            this.richTextBox_JsToCs.Size = new System.Drawing.Size(345, 132);
+            this.richTextBox_JsToCs.Size = new System.Drawing.Size(345, 193);
             this.richTextBox_JsToCs.TabIndex = 5;
             this.richTextBox_JsToCs.Text = "";
             // 
             // button_CsToJs
             // 
-            this.button_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button_CsToJs.Location = new System.Drawing.Point(725, 451);
+            this.button_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_CsToJs.Location = new System.Drawing.Point(661, 77);
             this.button_CsToJs.Name = "button_CsToJs";
             this.button_CsToJs.Size = new System.Drawing.Size(66, 23);
             this.button_CsToJs.TabIndex = 6;
@@ -117,17 +117,17 @@
             // 
             // textBox_CsToJs
             // 
-            this.textBox_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_CsToJs.Location = new System.Drawing.Point(725, 426);
+            this.textBox_CsToJs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_CsToJs.Location = new System.Drawing.Point(661, 52);
             this.textBox_CsToJs.Name = "textBox_CsToJs";
             this.textBox_CsToJs.Size = new System.Drawing.Size(346, 19);
             this.textBox_CsToJs.TabIndex = 8;
             // 
             // label_CsToJsReturn
             // 
-            this.label_CsToJsReturn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_CsToJsReturn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_CsToJsReturn.AutoSize = true;
-            this.label_CsToJsReturn.Location = new System.Drawing.Point(727, 481);
+            this.label_CsToJsReturn.Location = new System.Drawing.Point(663, 107);
             this.label_CsToJsReturn.Name = "label_CsToJsReturn";
             this.label_CsToJsReturn.Size = new System.Drawing.Size(90, 12);
             this.label_CsToJsReturn.TabIndex = 9;
@@ -135,40 +135,18 @@
             // 
             // richTextBox_CsToJsReturn
             // 
-            this.richTextBox_CsToJsReturn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox_CsToJsReturn.Location = new System.Drawing.Point(727, 497);
+            this.richTextBox_CsToJsReturn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox_CsToJsReturn.Location = new System.Drawing.Point(663, 123);
             this.richTextBox_CsToJsReturn.Name = "richTextBox_CsToJsReturn";
             this.richTextBox_CsToJsReturn.Size = new System.Drawing.Size(344, 87);
             this.richTextBox_CsToJsReturn.TabIndex = 10;
             this.richTextBox_CsToJsReturn.Text = "";
             // 
-            // mapBox1
-            // 
-            this.mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.None;
-            this.mapBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.mapBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.mapBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.mapBox1.CustomTool = null;
-            this.mapBox1.FineZoomFactor = 10D;
-            this.mapBox1.Location = new System.Drawing.Point(395, 47);
-            this.mapBox1.MapQueryMode = SharpMap.Forms.MapBox.MapQueryType.LayerByIndex;
-            this.mapBox1.Name = "mapBox1";
-            this.mapBox1.QueryGrowFactor = 5F;
-            this.mapBox1.QueryLayerIndex = 0;
-            this.mapBox1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
-            this.mapBox1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
-            this.mapBox1.ShowProgressUpdate = false;
-            this.mapBox1.Size = new System.Drawing.Size(672, 344);
-            this.mapBox1.TabIndex = 11;
-            this.mapBox1.Text = "mapBox1";
-            this.mapBox1.WheelZoomMagnitude = -2D;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1083, 750);
-            this.Controls.Add(this.mapBox1);
+            this.ClientSize = new System.Drawing.Size(1019, 450);
             this.Controls.Add(this.richTextBox_CsToJsReturn);
             this.Controls.Add(this.label_CsToJsReturn);
             this.Controls.Add(this.textBox_CsToJs);
@@ -181,7 +159,6 @@
             this.Controls.Add(this.webView);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -200,7 +177,6 @@
         private System.Windows.Forms.TextBox textBox_CsToJs;
         private System.Windows.Forms.Label label_CsToJsReturn;
         private System.Windows.Forms.RichTextBox richTextBox_CsToJsReturn;
-        private SharpMap.Forms.MapBox mapBox1;
     }
 }
 
